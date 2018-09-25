6ffd017298a94dfdb5662da94ebaa18bd9ab1e26
youaresofunny <anlopan@gmail.com>
2018-09-25 16:33:22 +0500

    Replace recursive clone with sed script

--- builder.sh
+++ builder.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 rm -rf active/$1
-git clone --recursive -b $1 --single-branch git@github.com:youaresofunny/bitshares-community-ui.git active/$1
-cd active/$1 && docker build -t delivery-$1 . && mkdir dist && docker run --rm -v $PWD/dist:/usr/src/app/dist delivery-$1
+git clone -b $1 --single-branch https://github.com/youaresofunny/bitshares-community-ui.git active/$1
+cd active/$1
+sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
+git submodule init
+git submodule sync
+git submodule update && docker build -t delivery-$1 . && mkdir dist && docker run --rm -v $PWD/dist:/usr/src/app/dist delivery-$1
